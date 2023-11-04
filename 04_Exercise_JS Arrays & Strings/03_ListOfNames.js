183f9b647e74773789d54988436fba0bd7c07b0a
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-04 22:40:59 +0200

    ex. 4 added

---
+++ 04_Exercise_JS Arrays & Strings/03_ListOfNames.js
@@ -0,0 +1,14 @@
+function sortList(list) {
+    list.sort(
+        (a, b) => {
+            return a.localeCompare(b);
+        }
+    );
+    let index = 1;
+    for(const name of list){
+        console.log(`${index}.${name}`)
+        index++;
+    }
+}
+
+//sortList(["John", "Bob", "Christina", "Ema"]);
