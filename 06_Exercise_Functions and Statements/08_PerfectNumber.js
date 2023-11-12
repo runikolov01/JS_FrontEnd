787626f3ccdef8e34dca218ae397524bc4122d40
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-12 23:27:43 +0200

    08. Perfect Number

---
+++ 06_Exercise_Functions and Statements/08_PerfectNumber.js
@@ -0,0 +1,17 @@
+function isNumberPerfect(number) {
+    let sum = 0;
+    for (let i = 0; i < number; i++) {
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum === number) {
+        console.log("We have a perfect number!");
+    }
+    else {
+        console.log("It's not so perfect.");
+    }
+}
+
+isNumberPerfect(1236498);
