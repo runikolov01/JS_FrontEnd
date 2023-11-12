8ae3987f3c56cac65936f9ba558cc6747540b40c
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-12 18:01:00 +0200

    Update 01_SmallestNumber.js

--- 06_Exercise_Functions and Statements/01_SmallestNumber.js
+++ 06_Exercise_Functions and Statements/01_SmallestNumber.js
@@ -1,11 +1,12 @@
 function findSmallestNumber(first, second, third) {
-    let smallest = first;
-    if (second < smallest) {
-        smallest = second;
-    }
-    if (third < smallest) {
-        smallest = third;
-    }
-    console.log(smallest)
+    // let smallest = first;
+    // if (second < smallest) {
+    //     smallest = second;
+    // }
+    // if (third < smallest) {
+    //     smallest = third;
+    // }
+    // console.log(smallest);
+    console.log(Math.min(first, second, third));
 }
 findSmallestNumber(2, 5, 3);
