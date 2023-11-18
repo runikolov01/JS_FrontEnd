09cf456bb260fe57c69260788e8836b9baae6a6f
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-18 10:08:45 +0200

    Create 01_PersonInfo

---
+++ 07_Objects and Classes/01_PersonInfo.js
@@ -0,0 +1,8 @@
+function personInfo(firstName, lastName, age) {
+    let object = {};
+    object.firstName = firstName;
+    object.lastName = lastName;
+    object.age = age;
+
+    return object;
+}
