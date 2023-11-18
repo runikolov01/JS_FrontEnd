c946ad35578849a9f44ac904f7a6276ac5c25066
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-18 19:59:07 +0200

    Create 05. Phone Book

---
+++ 07_Objects and Classes/05_PhoneBook.js
@@ -0,0 +1,18 @@
+function getPhoneBook(array) {
+    const phoneBook = {};
+
+    for (const element of array) {
+        const [name, phone] = element.split(" ");
+        phoneBook[name] = phone;
+    }
+
+    for (const key in phoneBook) {
+        console.log(`${key} -> ${phoneBook[key]}`);
+    }
+}
+
+getPhoneBook(['Tim 0834212554',
+    'Peter 0877547887',
+    'Bill 0896543112',
+    'Tim 0876566344']
+);
