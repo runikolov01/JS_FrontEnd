8f31a597cbcdec5a7b81a63a322b2ad6c114944e
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-23 19:22:56 +0200

    07. Extract Parenthesis

--- 09_DOM_Introduction/05.JS-Front-End-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ 09_DOM_Introduction/05.JS-Front-End-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,16 @@
-function extract(content) {
+function extract(id) {
+    const text = document.getElementById(id).textContent;
+    const pattern = /\((.+?)\)/g;
+    let result = [];
 
+    let match = pattern.exec(text);
+    console.log(match);
+
+    while (match !== null) {
+        result.push(match[1]);
+        match = pattern.exec(text);
+    }
+
+    console.log(result.join('; '));
+    return result.join(';')
 }
