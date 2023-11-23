63b11c9a94ca5e7e79e72ee1ab8256f48a3355cc
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-23 14:13:30 +0200

    01. Edit Element

--- 09_DOM_Introduction/05.JS-Front-End-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
+++ 09_DOM_Introduction/05.JS-Front-End-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
@@ -1,3 +1,7 @@
-function editElement() {
-    // TODO
+function editElement(ref, match, replacer) {
+    const content = ref.textContent;
+    const editedText = content.replace(new RegExp(match, 'g'), replacer);
+    ref.textContent = editedText;
+
+    console.log(editedText);
 }
