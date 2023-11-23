cb604aa52d4c861b60fea9990d65224f1116b30f
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-23 16:08:04 +0200

    04. Show More

--- 09_DOM_Introduction/05.JS-Front-End-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
+++ 09_DOM_Introduction/05.JS-Front-End-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+    const hiddenTextElemet = document.getElementById('text');
+    hiddenTextElemet.style.display = 'inline';
+    const readMoreButtonElement = document.querySelector('#more');
+    readMoreButtonElement.style.display = 'none';
 }
