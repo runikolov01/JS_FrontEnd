cd30df39bf830c3584bb833b23bcf1eff5bcaeef
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-23 15:56:10 +0200

    02. Collect List

--- 09_DOM_Introduction/05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ 09_DOM_Introduction/05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,13 @@
 function extractText() {
-    // TODO
+    const items = document.getElementsByTagName('li');
+    const result = [];
+
+    for (const item of Array.from(items)) {
+        result.push(item.textContent);
+    }
+
+    const textAreaElement = document.getElementById('result');
+    textAreaElement.textContent = result.join("\n");
+
+    console.log('result', result);
 }
