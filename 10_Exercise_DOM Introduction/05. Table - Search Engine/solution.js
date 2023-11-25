41b8355b1069b1acdeb5cd4f6e78ab9488102fff
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-25 23:39:41 +0200

    05. Table – Search Engine

--- 10_Exercise_DOM Introduction/05. Table - Search Engine/solution.js
+++ 10_Exercise_DOM Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,16 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   let input = document.getElementById('searchField');
+   let tableRows = document.querySelectorAll('tbody tr');
 
+   function onClick() {
+      for (const row of tableRows) {
+         row.classList.remove('select');
+         if (input.value !== '' && row.innerHTML.includes(input.value)) {
+            row.className = 'select';
+         }
+      }
+      input.value = '';
    }
 }
