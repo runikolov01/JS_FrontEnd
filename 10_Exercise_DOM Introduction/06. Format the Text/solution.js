bc4d5b0fa60d681ae98fbde78538bae745612f27
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-26 00:50:15 +0200

    06. Format the Text

--- 10_Exercise_DOM Introduction/06. Format the Text/solution.js
+++ 10_Exercise_DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+  let rawText = document.getElementById('input').value;
+  let outputElement = document.getElementById('output');
+
+  let sentences = rawText.split('.');
+
+  sentences = sentences.filter(s => s.length > 0).map(s => s += '.');
+
+  while (sentences.length > 0) {
+    let p = document.createElement('p');
+    p.textContent = sentences.splice(0, 3).join('');
+    outputElement.appendChild(p);
+  }
 }
