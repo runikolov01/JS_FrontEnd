d61e3dd3136852f0a154c370ddbbc1a98a77cfee
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-26 21:15:18 +0200

    09. Number Convertor

--- 10_Exercise_DOM Introduction/09. Number Convertor/solution.js
+++ 10_Exercise_DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,39 @@
 function solve() {
+    const selectMenuTo = document.getElementById("selectMenuTo");
+    const binaryOption = document.createElement("option");
+    binaryOption.value = "binary";
+    binaryOption.text = "Binary";
+    const hexOption = document.createElement("option");
+    hexOption.value = "hexadecimal";
+    hexOption.text = "Hexadecimal";
+    selectMenuTo.appendChild(binaryOption);
+    selectMenuTo.appendChild(hexOption);
 
-    //TODO...
+    const convertButton = document.querySelector("button");
+    convertButton.addEventListener("click", convertNumber);
+
+    function convertNumber() {
+        const inputValue = document.getElementById("input").value;
+        const fromOption = document.getElementById("selectMenuFrom").value;
+        const toOption = document.getElementById("selectMenuTo").value;
+
+        let result;
+        if (toOption === "binary") {
+            result = decimalToBinary(inputValue);
+        } else if (toOption === "hexadecimal") {
+            result = decimalToHexadecimal(inputValue);
+        } else {
+            result = "Invalid conversion";
+        }
+
+        document.getElementById("result").value = result;
+    }
+
+    function decimalToBinary(decimalNumber) {
+        return parseInt(decimalNumber, 10).toString(2);
+    }
+
+    function decimalToHexadecimal(decimalNumber) {
+        return parseInt(decimalNumber, 10).toString(16).toUpperCase();
+    }
 }
