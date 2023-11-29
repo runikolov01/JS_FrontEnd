6819152f820c0d5b6691485a8a502b8ba13365a7
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-29 22:27:43 +0200

    02. Delete from Table

--- 11_DOM Manipulation and Events/02. Delete-From-Table/app.js
+++ 11_DOM Manipulation and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,20 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const inputElement = document.querySelector('input[name="email"]');
+    const tableRows = Array.from(document.querySelector("tbody").children);
+    const resultElement = document.querySelector("#result");
+    let isRemoved = false;
+
+    for (const row of tableRows) {
+        const emailCell = row.children[1];
+        if (emailCell.textContent === inputElement.value) {
+            row.remove();
+            isRemoved = true;
+        }
+    }
+
+    if (isRemoved) {
+        resultElement.textContent = "Deleted";
+    } else {
+        resultElement.textContent = "Not found.";
+    }
 }
