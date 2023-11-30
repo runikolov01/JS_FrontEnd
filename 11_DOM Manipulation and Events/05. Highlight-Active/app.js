3589e82ef265b96d3ca772d5148bdbf34298ea46
Rumen Nikolov <rumenhco2001@abv.bg>
2023-11-30 23:54:38 +0200

    05. Highlight Active

--- 11_DOM Manipulation and Events/05. Highlight-Active/app.js
+++ 11_DOM Manipulation and Events/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    const fields = Array.from(document.getElementsByTagName('input'));
+    for (const field of fields) {
+        field.addEventListener('focus', onFocus);
+        field.addEventListener('blur', onBlur);
+    }
+
+    function onFocus(e) {
+        const divParentElement = e.currentTarget.parentNode;
+        divParentElement.classList.add('focused');
+    }
+
+    function onBlur(e) {
+        const divParentElement = e.currentTarget.parentNode;
+        divParentElement.classList.remove('focused');
+    }
 }
