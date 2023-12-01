02d7d4a172c6b6f96997d128044ff142206891b2
Rumen Nikolov <rumenhco2001@abv.bg>
2023-12-02 00:06:03 +0200

    03. Locked Profile

--- 12_Exercise_DOM Manipulation and Events/03. Locked-Profile/app.js
+++ 12_Exercise_DOM Manipulation and Events/03. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.getElementsByTagName('button'));
+    buttons.forEach((button) => {
+        button.addEventListener('click', showMoreInfo)
+    });
+
+    function showMoreInfo(event) {
+        let btn = event.target; // гарантира, че в зависимост от бутона, който ще натиснем, ще се изпълни функцията върху точно неговия parent element
+        let profile = btn.parentElement;
+        let children = Array.from(profile.children);
+
+        let unlocked = children[4];
+        let additionalInfo = children[9];
+
+        if (!unlocked.checked) {
+            return;
+        }
+
+        if (btn.textContent === 'Show more') {
+            additionalInfo.style.display = 'block';
+            btn.textContent = 'Hide it';
+        } else if (btn.textContent === 'Hide it') {
+            additionalInfo.style.display = 'none';
+            btn.textContent = 'Show more';
+        }
+    }
 }
