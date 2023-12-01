9f1719cffae166878685d85c0a063cc3d6f33e48
Rumen Nikolov <rumenhco2001@abv.bg>
2023-12-01 23:06:31 +0200

    01. Sections

--- 12_Exercise_DOM Manipulation and Events/01. Sections/script.js
+++ 12_Exercise_DOM Manipulation and Events/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let mainDiv = document.getElementById('content');
+   for (const word of words) {
+      let div = document.createElement('div');
+      let paragraph = document.createElement('p');
+
+      paragraph.textContent = word;
+      paragraph.style.display = 'none';
+
+      div.appendChild(paragraph);
+      mainDiv.appendChild(div);
+
+      div.addEventListener('click', () => {
+         paragraph.style.display = '';
+      })
+   }
 }
