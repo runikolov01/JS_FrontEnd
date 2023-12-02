041fc6b0678ed18ef20310a09b2f0a04714f4bde
Rumen Nikolov <rumenhco2001@abv.bg>
2023-12-02 11:08:08 +0200

    05. Encode and Decode Messages

--- 12_Exercise_DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
+++ 12_Exercise_DOM Manipulation and Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,37 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeTextBox = document.getElementsByTagName('textarea')[0];
+    let encodeBtn = document.getElementsByTagName('button')[0];
+
+    let decodeTextBox = document.getElementsByTagName('textarea')[1];
+    let decodeBtn = document.getElementsByTagName('button')[1];
+
+    encodeBtn.addEventListener('click', encodeMessage);
+    decodeBtn.addEventListener('click', decodeMessage);
+
+    function encodeMessage() {
+        let message = encodeTextBox.value;
+        let newMessage = '';
+
+        for (let index = 0; index < message.length; index++) {
+            let newText = String.fromCodePoint(message[index].charCodeAt(0) + 1);
+            newMessage += newText;
+        }
+
+        decodeTextBox.value = newMessage;
+        encodeTextBox.value = '';
+        document.getElementsByTagName('button')[1].disabled = false;
+    }
+
+    function decodeMessage() {
+        let message = decodeTextBox.value;
+        let newMessage = '';
+
+        for (let index = 0; index < message.length; index++) {
+            let newText = String.fromCodePoint(message[index].charCodeAt(0) - 1);
+            newMessage += newText;
+        }
+
+        decodeTextBox.value = newMessage;
+        document.getElementsByTagName('button')[1].disabled = true;
+    }
 }
