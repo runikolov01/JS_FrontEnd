a56d91f0399a055f0c34376e741b6d3fba63b07e
Rumen Nikolov <rumenhco2001@abv.bg>
2023-12-14 15:02:38 +0200

    02. Messenger

--- 15_Exercise HTTP and AJAX/02.Messenger/app.js
+++ 15_Exercise HTTP and AJAX/02.Messenger/app.js
@@ -1,5 +1,34 @@
 function attachEvents() {
-    // TODO:
+    const baseUrl = 'http://localhost:3030/jsonstore/messenger';
+    const [nameInput, contentInput, sendButton, refreshButton] = document.getElementsByTagName('input');
+    const messagesTextArea = document.getElementById('messages');
+
+    sendButton.addEventListener('click', async () => {
+        const messageFromat = {
+            author: nameInput.value,
+            content: contentInput.value,
+        };
+        let isValidMessage = nameInput.value !== '' && contentInput.value !== '';
+        if (isValidMessage) {
+            await fetch(baseUrl, {
+                method: 'POST',
+                body: JSON.stringify(messageFromat)
+            });
+        }
+        nameInput.value = '';
+        contentInput.value = '';
+    })
+
+    refreshButton.addEventListener('click', async () => {
+        const response = await fetch(baseUrl);
+        const messages = await response.json();
+        const messagesToDisplay = [];
+
+        for (const messageInfo of Object.values(messages)) {
+            messagesToDisplay.push(`${messageInfo.author}: ${messageInfo.content}`);
+        }
+        messagesTextArea.textContent = messagesToDisplay.join('\n');
+    })
 }
 
 attachEvents();
