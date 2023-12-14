7e7367a5c6b79c1369747a33b717f039d2f84bf8
Rumen Nikolov <rumenhco2001@abv.bg>
2023-12-14 23:00:08 +0200

    03. Phonebook

--- 15_Exercise HTTP and AJAX/03.Phonebook/app.js
+++ 15_Exercise HTTP and AJAX/03.Phonebook/app.js
@@ -1,5 +1,52 @@
 function attachEvents() {
-    // TODO:
+    const baseURL = 'http://localhost:3030/jsonstore/phonebook';
+
+    document.getElementById('btnLoad').addEventListener('click', getAllPhones);
+    document.getElementById('btnCreate').addEventListener('click', createContact);
+
+    async function getAllPhones() {
+        const phonesInfo = await (await fetch(baseURL)).json();
+        const phoneBook = document.getElementById('phonebook');
+
+        Object.values(phonesInfo).forEach((contact) => {
+            const li = document.createElement('li');
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+
+            li.textContent = `${contact.person}: ${contact.phone}`;
+            li.appendChild(deleteButton);
+            phoneBook.appendChild(li);
+
+            deleteButton.addEventListener('click', deleteContat);
+
+            function deleteContat() {
+                const id = contact._id;
+                fetch(`http://localhost:3030/jsonstore/phonebook/${id}`, {
+                    method: 'DELETE',
+                });
+                li.remove();
+            }
+        })
+    }
+
+    function createContact() {
+        const person = document.getElementById('person').value;
+        const phone = document.getElementById('phone').value;
+
+        fetch(baseURL, {
+            method: 'POST',
+            body: JSON.stringify({
+                person,
+                phone
+            })
+        })
+            .then((response) => response.json())
+            .then(() => {
+                getAllPhones();
+                person.value = '';
+                phone.value = '';
+            })
+    }
 }
 
 attachEvents();
